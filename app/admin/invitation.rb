72c503a563b6cdd0c5792fbf9817858f513c2943
Graham Melcher <melcher@gmail.com>
2015-02-04 20:32:15 -0800

    admin seems to be working

---
+++ app/admin/invitation.rb
@@ -0,0 +1,36 @@
+ActiveAdmin.register Invitation do
+  includes :response
+  scope :no_response, :responded
+
+  index do
+    selectable_column
+    column :name, sortable: 'invitation.name' do |response|
+      link_to response.name, admin_response_path(response)
+    end
+    column :party_size
+    column :response, sortable: 'response.responded_with' do |invitation|
+      if invitation.response
+        link_to invitation.response.responded_with, admin_response_path(invitation.response)
+      end
+    end
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
+end
